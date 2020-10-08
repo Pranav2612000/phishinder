18861bcb280920f856cfc4daa6d8579e57ccc405
Pranav <pranav2000joglekar@gmail.com>
2020-10-08 12:38:57 +0530

    initial commit

---
+++ phishinder.py
@@ -0,0 +1,281 @@
+from sklearn.externals import joblib
+import dns
+import dns.resolver
+import whois
+import urlunshort3
+from dateutil.relativedelta import relativedelta
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+import datetime
+import favicon
+import requests
+import os
+import csv
+import json
+import pandas as pd
+from random import randint
+import shutil
+from enum import Enum
+import warnings
+warnings.filterwarnings("ignore")
+
+# ***************************Logging related imports and setting**************************************
+import time
+import logging
+import sys
+from logging.handlers import TimedRotatingFileHandler
+
+def getlogger(fileName = "newsScrapelog"):
+    # format the log entries
+    formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
+
+    my_path = os.path.abspath(os.path.dirname(__file__))
+    if(not os.path.exists(os.path.join(my_path, "logs"))):
+        os.makedirs(os.path.join(my_path, "logs"))
+
+    logFilename = "logs/" + fileName
+    handler = TimedRotatingFileHandler(os.path.join(my_path, logFilename),
+                                    when="midnight",
+                                    backupCount=5)
+    handler.setFormatter(formatter)
+    logger = logging.getLogger(__name__)
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
+    return logger
+
+
+phisher = joblib.load("./phishing_detection.pkl")
+
+site = [[]];
+for i in range(30):
+      site[0].append(0)
+
+#url = "https://1drv.ms/xs/s%21AigEtNdrgPjxaXe8pK9BSeEKf4E?wdFormId=%7B1407D668%2D675D%2D4568%2DA543%2DDC6C703FA6F0%7D"
+#url = "https://dimasotoykeras.evjzghsp.com/?sign"
+#url = "http://anazzonm-ehpq.co/pc/"
+#url = "https://mail-109887.square.site/"
+#url = "https://pranavjoglekarcodes.web.app"
+#url = "https://www.google.com"
+#url = "https://www.amazon.com"
+#url = "https://outlook.live.com"
+url = input()
+logger = getlogger("out")
+
+path_start = url.find(':') + 3
+path = url[path_start:]
+try:
+    html = urlopen(url)
+    bs = BeautifulSoup(html, 'html.parser')
+except:
+    logger.info("Error opening page")
+    bs = None
+
+try:
+    domain = whois.query(path)
+except:
+    domain = None
+logger.info(path)
+try:
+    dnsresult = dns.resolver.query(path, 'A')
+    for ipval in dnsresult:
+        logger.info('IP:', ipval.to_text())
+except:
+    dnsresult = None
+
+if(domain != None):
+    logger.info(domain.__dict__)
+logger.info(dnsresult)
+
+
+#has IP:
+name = url.split("/")[2];
+count = 0;
+parts = name.split(".")
+for string in parts:
+  if(string.isnumeric()):
+    count = count + 1
+    if(count > 2):
+      site[0][0] = -1
+    else:
+      site[0][0] = 1
+      logger.info(site[0][0])
+
+#length of url
+if(len(url) > 60):
+  site[0][1] = -1
+if(len(url) < 30):
+  site[0][1] = 1
+
+#Shortening Service - Either check for 301 code or known names in url
+services = ["bit.ly", "is.gd", "t.co", "tinyurl.com", "tiny.cc"]
+site[0][1] = 1
+for service in services:
+  if(service in url):
+    site[0][1] = -1
+
+# having @ symbol
+if("@" in url):
+  site[0][3] = -1
+else:
+  site[0][3] = 1
+
+# having double slash
+if(path.find("//") == -1):
+  site[0][4] = 1
+else:
+  site[0][4] = -1
+
+#presence of -
+if(path.find("-") == -1):
+  site[0][5] = 1
+else:
+  site[0][5] = -1
+
+#having subdomain
+site[0][6] = -1
+
+#having https
+if(url[:5] == "https"):
+  site[0][7] = 1
+else:
+  site[0][7] = -1
+
+#domain registration length
+last_year_date = datetime.datetime.now() - relativedelta(years=1)
+if(domain != None and domain.creation_date > last_year_date):
+  site[0][8] = -1
+else:
+  site[0][8] = 1
+
+# favicon from diffrent url
+try:
+    icons = favicon.get(url)
+    logger.info(icons)
+    if(icons != None and icons != [] and (path in icons[0].url or name in icons[0].url)):
+      site[0][9] = 1
+    else:
+      site[0][9] = -1
+except:
+    logger.info("Error opening url")
+
+# are all ports open
+site[0][10] = 0
+
+# is https-token in path
+if "https" in path:
+  site[0][11] = -1
+else:
+  site[0][11] = 1
+
+#images from same domain
+try:
+    count = 0
+    vcount = 0
+    images = bs.find_all('img')
+    for image in images:
+        count = count + 1
+        if(image['src'] != '' and image['src'] != None and (image['src'][0] == '/' or path in image['src'])):
+            vcount = vcount + 1
+            continue
+    if(count != 0 and ((vcount / count) * 100 > 30)):
+        site[0][12] = 1
+    else:
+        site[0][12] = -1
+except:
+    logger.info("error opening images")
+    site[0][12] = -1
+
+#url of anchor
+try:
+    count = 0
+    vcount = 0
+    anchors = bs.find_all('a')
+    for anchor in anchors:
+        count = count + 1
+        if(anchor['src'] != '' and anchor['src'] != None and (anchor['src'][0] == '/' or path in anchor['src'])):
+            vcount = vcount + 1
+            continue
+    if(count != 0 and ((vcount / count) * 100 > 60)):
+        site[0][13] = 1
+    else:
+        site[0][13] = -1
+except:
+    logger.info("error ipening as")
+    site[0][13] = -1
+
+#links in tags
+try:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text)
+    metas = soup.find_all('meta')
+    if(metas != None or metas != [] and(len(metas) > 5)):
+        site[0][14] = 1
+    else:
+        site[0][14] = -1
+except:
+    logger.info("error in getting meta")
+    site[0][14] = -1
+
+#server form handler
+site[0][15] = -1
+
+#client-side submission to email using mailto
+site[0][16] = -1
+
+#check if whois exists
+try:
+    if(domain != None):
+      site[0][17] = 1
+    else:
+      site[0][17] = -1
+except:
+    logger.info("No whois found")
+    site[0][17] = -1
+
+#no of redirects
+try:
+    r = requests.head(url)
+    if(str(r.status_code)[0] == '2'):
+      site[0][18] = -1
+    else:
+      site[0][18] = +1
+
+except Exception as e:
+    logger.info(e)
+    logger.info("Error finding redirects")
+
+# does onmouseover change statusbar
+site[0][19] = 0
+
+# is right click disabled
+site[0][20] = 0
+
+# popup window contains text forms
+site[0][21] = -1
+
+#iframe frameBorders
+site[0][22] = -1
+
+# no dns records
+if(dnsresult != None):
+    site[0][25] = 1
+else:
+    site[0][25] = -1
+
+# website traffic
+site[0][26] = -1
+
+# page rank value 
+site[0][27] = -1
+
+# in google index
+site[0][28] = -1
+
+# no of links pointing to them
+site[0][29] = -1
+
+# found in stastical reports
+results = ["Malicious", "Safe"]
+logger.info(site)
+print(results[int(phisher.predict(site))])
